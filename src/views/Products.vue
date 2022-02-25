d4a29865b4d6bc76975aa607f7856c8fe6ee5b82
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-02-25 08:59:48 -0800

    Update to single page

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -123,30 +123,34 @@
                     class="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
                 >
                     <div v-for="product in relatedProducts" :key="product.id">
-                        <div class="relative">
-                            <div class="relative w-full h-72 rounded-lg overflow-hidden">
-                                <img
-                                    :src="product.imageSrc"
-                                    :alt="product.imageAlt"
-                                    class="w-full h-full object-center object-cover"
-                                />
-                            </div>
-                            <div class="relative mt-4">
-                                <h3 class="text-sm font-medium text-gray-900">{{ product.name }}</h3>
-                                <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
-                            </div>
-                            <div
-                                class="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden"
-                            >
+                        <router-link :to="'/products/' + product.id">
+                            <div class="relative">
+                                <div class="relative w-full h-72 rounded-lg overflow-hidden">
+                                    <img
+                                        :src="product.image"
+                                        :alt="product.imageAlt"
+                                        class="w-full h-full inset-0 object-center object-contain"
+                                    />
+                                </div>
+                                <div class="relative mt-4">
+                                    <h3
+                                        class="text-sm font-medium text-gray-900"
+                                    >{{ product.title }}</h3>
+                                    <p class="mt-1 text-sm text-gray-500">{{ product.color }}</p>
+                                </div>
                                 <div
-                                    aria-hidden="true"
-                                    class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
-                                />
-                                <p
-                                    class="relative text-lg font-semibold text-white"
-                                >{{ product.price }}</p>
+                                    class="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden"
+                                >
+                                    <div
+                                        aria-hidden="true"
+                                        class="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
+                                    />
+                                    <p
+                                        class="relative text-lg font-semibold text-white"
+                                    >{{ product.price }}</p>
+                                </div>
                             </div>
-                        </div>
+                        </router-link>
                         <div class="mt-6">
                             <a
                                 :href="product.href"
@@ -188,18 +192,7 @@ import {
 import { StarIcon } from '@heroicons/vue/solid'
 
 const product = ref({})
-const relatedProducts = [
-    {
-        id: 1,
-        name: 'Zip Tote Basket',
-        color: 'White and black',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-03-related-product-01.jpg',
-        imageAlt: 'Front of zip tote bag with white canvas, black canvas straps and handle, and black zipper pulls.',
-        price: '$140',
-    },
-    // More products...
-]
+const relatedProducts = ref([])
 
 export default {
     components: {
@@ -232,8 +225,18 @@ export default {
             axios.get("https://fakestoreapi.com/products/" + productId.value)
                 .then((res) => {
                     product.value = res.data
-                    console.log(product.value.rating.rate);
+
+                    getCategory(product.value.category)
                 })
+        }
+
+        const getCategory = (categoryName) => {
+            console.log(categoryName)
+
+            axios.get("https://fakestoreapi.com/products/category/" + categoryName).then((res) => {
+                relatedProducts.value = res.data
+                console.log(relatedProducts)
+            })
         }
 
         const totalRatings = 5
@@ -260,7 +263,8 @@ export default {
             getProduct,
             getRating,
             totalRatings,
-            showColor
+            showColor,
+
         }
     },
 
