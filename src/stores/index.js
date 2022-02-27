e1f45e23d318e004211581b1afdc5dff25c6341e
Efosa Uyi-Idahor <efosa.uyi@gmail.com>
2022-02-27 04:53:09 -0800

    Installing of Vuex

---
+++ src/stores/index.js
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      color: "bg-yellow-600",
+    };
+  },
+  mutations: {},
+});
+
+export default store;
